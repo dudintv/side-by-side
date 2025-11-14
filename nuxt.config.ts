420a92a352d9a53effbbd9f525ed0dec138e3724
Dmitry Dudin <dudin.tv@gmail.com>
2025-11-14 12:51:24 +0100

    feat: add Google Analytics Tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/ui', '@nuxt/image', 'nuxt-gtag'],
   css: ['~/assets/css/main.css'],
   content: {
     build: {
@@ -10,5 +10,9 @@ export default defineNuxtConfig({
         highlight: false,
       },
     },
+  },
+  gtag: {
+    id: 'G-WM93BP36VS',
+    enabled: import.meta.env.PROD,
   },
 });
