3842d039c0f3db7e83f9814f40a040a830f80294
Dmitry Dudin <dudin.tv@gmail.com>
2025-11-27 00:40:42 +0100

    feat: optimize gaps in the header

--- app/components/FrameworkSelector.vue
+++ app/components/FrameworkSelector.vue
@@ -12,9 +12,14 @@ const variant = computed(() => framework.value.variants?.[0] || null);
 <template>
   <div class="flex items-center transition-[gap]" :class="[isSmall ? 'gap-2' : 'gap-2 sm:gap-4']">
     <FrameworkLogo :name :size="isSmall ? 25 : 30" />
-    <div class="flex items-baseline max-lg:flex-col" :class="[isSmall ? 'gap-0.5' : 'gap-1 lg:gap-2']">
-      <span class="shrink-0 text-lg font-semibold leading-none"> {{ displayName }} {{ version }} </span>
-      <span v-if="variant" class="text-xs md:text-sm lg:text-base font-medium leading-none shrink-0 opacity-60">
+    <div class="flex items-baseline max-lg:flex-col" :class="[isSmall ? 'gap-0.5 lg:gap-2' : 'gap-1 lg:gap-4']">
+      <span class="shrink-0 md:text-lg font-semibold leading-none whitespace-nowrap">
+        {{ displayName }} {{ version }}
+      </span>
+      <span
+        v-if="variant"
+        class="text-tiny sm:text-xs md:text-sm lg:text-base font-medium leading-none shrink-0 opacity-60"
+      >
         {{ variant }}
       </span>
     </div>
